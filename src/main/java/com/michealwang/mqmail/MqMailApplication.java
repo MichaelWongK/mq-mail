56b48b950dc422f756d77e1988ba7228996191cd
micheal.wang <wangmk13@163.com>
2020-09-15 17:46:56 +0800

    feat: init

---
+++ src/main/java/com/michealwang/mqmail/MqMailApplication.java
@@ -0,0 +1,13 @@
+package com.michealwang.mqmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqMailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/michealwang/mqmail/MqMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michealwang.mqmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
